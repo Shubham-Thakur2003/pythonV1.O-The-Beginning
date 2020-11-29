7446078d162d4c1b0723fa68b16ae61f77e6914f
Shubham <std11491@gmail.com>
2020-11-29 12:42:56 +0530

    Initial Commit

---
+++ Mini Dictionary.py
@@ -0,0 +1,8 @@
+#Mini - Dictionary
+print("welcome to our dictionary")
+dictionary = {"set":"well define collection of objects","anonymous":"hacker", "python":"computer language","lol":"laugh out loud"}
+print("write your word here")
+search = input()
+print("The required meaning is:")
+print(dictionary[search])
+print("I hope this helps you, Thanks for using Dictionary") 
